207f52e324185f865c1372d696085f59ea43cb44
Ashwatha Bhaskar <ashwatha.bhaskar@athlon.com>
2025-12-09 22:05:41 +0100

    clean

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "key_vault_id" {
-  description = "The ID of the Azure Key Vault."
-  value       = azurerm_key_vault.main.id
-}
-
-output "key_vault_uri" {
-  description = "The URI of the Azure Key Vault."
-  value       = azurerm_key_vault.main.vault_uri
-}
