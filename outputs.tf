91dd97e32a8d6e57afdb7bdef281b371d2ce8a23
Ashwa <ashwajce@gmail.com>
2025-12-10 15:33:36 +0100

    CloudForgeX: Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,14 @@ output "resource_group_name" {
   description = "The name of the Resource Group where the Key Vault is created."
   value       = azurerm_resource_group.kv_rg.name
 }
+
+output "storage_account_id" {
+  description = "The ID of the Azure Storage Account."
+  value       = azurerm_storage_account.main.id
+}
+
+output "storage_account_primary_access_key" {
+  description = "The primary access key for the Azure Storage Account."
+  value       = azurerm_storage_account.main.primary_access_key
+  sensitive   = true
+}
