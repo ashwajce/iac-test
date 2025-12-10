b9d0db3d23389367aef539e20c5cf29e13db0fb3
Ashwa <ashwajce@gmail.com>
2025-12-10 16:33:29 +0100

    CloudForgeX: Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -23,3 +23,14 @@ output "storage_account_primary_access_key" {
   value       = azurerm_storage_account.main.primary_access_key
   sensitive   = true
 }
+
+output "new_storage_account_id" {
+  description = "The ID of the new Azure Storage Account."
+  value       = azurerm_storage_account.secondary.id
+}
+
+output "new_storage_account_primary_access_key" {
+  description = "The primary access key for the new Azure Storage Account."
+  value       = azurerm_storage_account.secondary.primary_access_key
+  sensitive   = true
+}
