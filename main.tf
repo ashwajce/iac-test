8d48d5f0c13c9ffaaf4347cbdd4f67a0b5c5dc0a
Ashwa <ashwajce@gmail.com>
2025-12-10 15:33:35 +0100

    CloudForgeX: Update main.tf

--- main.tf
+++ main.tf
@@ -14,16 +14,16 @@ provider "azurerm" {
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_resource_group" "kv_rg" {
-  name     = "${var.resource_group_name}"
-  location = "${var.region}"
+  name     = var.resource_group_name
+  location = var.region
 }
 
 resource "azurerm_key_vault" "main" {
-  name                = "${var.key_vault_name}"
+  name                = var.key_vault_name
   location            = azurerm_resource_group.kv_rg.location
   resource_group_name = azurerm_resource_group.kv_rg.name
   tenant_id           = data.azurerm_client_config.current.tenant_id
-  sku_name            = "${var.sku_name}"
+  sku_name            = var.sku_name
 
   access_policy {
     tenant_id = data.azurerm_client_config.current.tenant_id
@@ -76,4 +76,23 @@ resource "azurerm_key_vault" "main" {
   # Optional: Enable soft delete and purge protection
   soft_delete_retention_days = 7
   purge_protection_enabled   = false # Set to true for higher security environments
+}
+
+resource "azurerm_storage_account" "main" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.kv_rg.name
+  location                 = azurerm_resource_group.kv_rg.location
+  account_tier             = var.storage_account_tier
+  account_replication_type = var.storage_account_replication_type
+  account_kind             = var.storage_account_kind
+
+  # Enforce HTTPS traffic only for security best practice
+  enable_https_traffic_only = true
+
+  # Optional: Enable blob soft delete
+  blob_properties {
+    delete_retention_policy {
+      days = 7
+    }
+  }
 }
